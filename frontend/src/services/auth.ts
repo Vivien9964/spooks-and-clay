edc1c92625077574c8910f8aa139d4f741117e31
Vivien99964 <mecserikvivien2@gmail.com>
2026-06-24 16:16:49 +0300

    solved unused params error

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -3,7 +3,7 @@ import type { LoginPayload, RegisterPayload } from "@/types/user"
 import type { AuthResponse } from "@/types/api"
 
 
-export async function loginUser(payload: LoginPayload): Promise<AuthResponse> {
+export async function loginUser(_payload: LoginPayload): Promise<AuthResponse> {
     return {
         token: "hggdg5d54d5d55dssjhsysujs8s9s6s6s5ds6d5cd56c56d",
         user: { id: 1, name: "John Doe", email: "john.doe@example.com", role: "customer", createdAt: new Date().toISOString() }
@@ -11,7 +11,7 @@ export async function loginUser(payload: LoginPayload): Promise<AuthResponse> {
 }
 
 
-export async function registerUser(payload: RegisterPayload): Promise<AuthResponse> {
+export async function registerUser(_payload: RegisterPayload): Promise<AuthResponse> {
     return {
         token: "hggdg5d54d5d55dssjhsysujs8s9s6s6s5ds6d5cd56c56d",
         user: { id: 1, name: "John Doe", email: "john.doe@example.com", role: "customer", createdAt: new Date().toISOString() }
